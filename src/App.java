fb674985c6e5d42951287f96a410523deb0a8370
Joao Vitor Garcia Andrade <1142437@sga.pucminas.br>
2022-03-25 18:17:28 -0300

    correção leitor

--- src/App.java
+++ src/App.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 public class App {
     static final String nomeArquivo = "ARQUIVOS.TXT";
     static String path = "AED_20_MiniGoogle_Etapa1_2022.txt";
-    static int contador;
 
     static void loadFile() throws IOException {
         Scanner file = new Scanner(new File(path));
@@ -17,33 +16,34 @@ public class App {
         file.close();
     }
 
-    //Metodo responsavel por entrar vários arquivos e armazenar seus temos
-    public static Termo[] carregarDadosMultiplos() throws FileNotFoundException {
-        contador = 0;
+    // Metodo responsavel por entrar vários arquivos e armazenar seus temos
+    public static String[] carregarDadosMultiplos() throws FileNotFoundException {
         File arqQuantidade = new File(nomeArquivo);
         Scanner leitor = new Scanner(arqQuantidade);
         int qntArqvs = Integer.parseInt(leitor.nextLine());
         String[] arquivos = new String[qntArqvs];
-        //for entra no arquivo que armazena o nome de todos outros arquivos então cria um vetor de string com o nome de todos arquivos usados
+        String[] dados = new String[1000];
+        int contador = 0;
+
+        // for entra no arquivo que armazena o nome de todos outros arquivos então cria
+        // um vetor de string com o nome de todos arquivos usados
         for (int i = 0; i < qntArqvs; i++) {
-            String dados = leitor.nextLine();
-            arquivos[i] = dados;
+            arquivos[i] = leitor.nextLine();
         }
-        
-        ArrayList<Termo> termos = new ArrayList<Termo>();       //Metodo que achei que seria possivel criar um vetor de tamanho indefinido
-        //foreach que passar por todos arquivos então adiciona na classe *Termo* as entradas
-        for (String string : arquivos) {
-            File arquivo = new File(arquivos[contador]);
+
+        // foreach que passar por todos arquivos então adiciona na classe *Termo* as
+        // entradas
+        for (String leitorArquivo : arquivos) {
+            File arquivo = new File(leitorArquivo);
             Scanner leitorTermo = new Scanner(arquivo);
-            int qntTermos = Integer.parseInt(leitorTermo.nextLine());
 
-            String[] dados = leitor.nextLine().split(";");
-
+            dados[contador] = leitorTermo.nextLine();
+            leitorTermo.close();
             contador++;
         }
 
         leitor.close();
-        return termos;
+        return dados;
     }
 
     public static void limparTela() {
@@ -52,6 +52,14 @@ public class App {
     }
 
     public static void main(String[] args) throws Exception {
+        String[] dados = new String[1000];
+        limparTela();
+        dados = carregarDadosMultiplos();
+        for (String dado : dados) {
+            if (dado != null) {
+                System.out.println(dado);
+            }
+        }
         loadFile();
     }
 }
