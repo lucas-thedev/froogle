578e069630f6f46dd1c829772a7736a06c5e3b65
unknown <jaao-web>
2022-05-02 00:26:41 -0300

    trabalhando menu

--- src/App.java
+++ src/App.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -8,15 +9,15 @@ public class App {
     static final String nomeArquivoGravar = "DADOS.TXT";
     static String path = "AED_20_MiniGoogle_Etapa1_2022.txt";
 
-    static void salvarDados(ArrayList<Termo> dados) throws IOException{
+    static void salvarDados(ArrayList<Termo> dados) throws IOException {
         File arquivo = new File(nomeArquivoGravar);
         FileWriter escritor = new FileWriter(arquivo, false);
 
-        for(int i=0; i<dados.size(); i++){
+        for (int i = 0; i < dados.size(); i++) {
             String paraGravar = formatTermo(dados.get(i));
-            escritor.append(paraGravar+"\n");
+            escritor.append(paraGravar + "\n");
         }
-        
+
         escritor.close();
     }
 
@@ -29,19 +30,76 @@ public class App {
         System.out.flush();
     }
 
+    public  static void pausa(Scanner teclado){
+        System.out.println("\nEnter para continuar.");
+        teclado.nextLine();
+    }
+
     public static void main(String[] args) throws Exception {
         limparTela();
         Documento[] documentos = LoadData.ReadFiles(nomeArquivo);
         Termo[] termos = LoadData.LoadTermos(documentos);
 
+        Scanner menu = new Scanner(System.in);
+        int opcao;
+        do {
+            limparTela();
+            System.out.print("----------Froogle----------|\n\n");
+            System.out.print("|--------------------------|\n");
+            System.out.print("| Opção 1 - Consultar novo termo\n");
+            System.out.print("| Opção 2 - Inserir novo termo\n");
+            System.out.print("| Opção 3 - Exibir termos (decrescente por frequência)\n");
+            System.out.print("| Opção 4 - Sair\n");
+            System.out.print("|---------------------------|\n");
+            System.out.print("Digite uma opção: ");
 
-        for (Termo termo : termos) {
-            if (termo != null) {
-                System.out.println(termo.termo);
+            opcao = menu.nextInt();
+            switch (opcao) {
+                case 1:
+                    System.out.print("\nDigite o Termo que deseja pesquisar: ");
+                    Scanner termoPorcurado = new Scanner(System.in);
+                    String termoPorcurado_ = termoPorcurado.nextLine();
+                    int termoID = LoadData.searchTermo(termos, termoPorcurado_);
+                    if (termoID == -1) {
+                        System.out.print("Termo não existe.");
+                    } else {
+                        System.out.print("\nO termo: " + termos[termoID].termo);
+                        if (termos[termoID].docCounter == 0) {
+                            System.out.print(", não aparece em algum documento.");
+                        } else {
+                            System.out.print(", aparece nos docuemntos: \n");
+                            for (int i = 0; i < termos[termoID].docCounter; i++) {
+                                System.out.print(termos[termoID].documentos[i] + ", ");
+                            }
+                        }
+                    }
+                    pausa(termoPorcurado);
+                    break;
+
+                case 2:
+                    System.out.print("\nDigite o Termo que deseja inserir: (FALTANDO*********)");
+                    Scanner termoInserir = new Scanner(System.in);
+                    String termoInserir_ = termoInserir.nextLine();
+                    pausa(termoInserir);
+                    break;
+
+                case 3:
+                    System.out.print("\nExibir todos Termos (POR FREQUENCIA. FALTANDO********)\n");
+                    for (Termo termo : termos) {
+                        if (termos != null) {
+                            System.out.println(termo.termo);
+                        }
+                    }
+                    break;
+
+                default:
+                    System.out.print("\nOpção Inválida!");
+                    break;
+
+                case 4:
+                    System.out.print("\nAté logo!");
+                    menu.close();
             }
-            
-        }
-
-
+        } while (opcao != 4);
     }
 }
